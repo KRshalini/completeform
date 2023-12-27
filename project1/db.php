00381b530da90737767f1542835beb5c4ed590fb
KRshalini <114220850+KRshalini@users.noreply.github.com>
2023-12-27 10:52:33 +0530

    Update db.php

--- project1/db.php
+++ project1/db.php
@@ -2,7 +2,7 @@
 $db_host = 'localhost';
 $db_user = "root";
 $db_pass = "";
-$db_name = "fuck";
+$db_name = "database name";
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if ($con) {
